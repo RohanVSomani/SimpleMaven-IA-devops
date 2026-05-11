5724bce1f1e34cfd0c09b9cf84500516779d9500
RohanVSomani <rohanvsomani@gmail.com>
2026-05-11 09:41:52 +0530

    First Commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "S G Modhak Raj" );
+        System.out.println( "Usn  1BI23CS184" );
+    }
+}
